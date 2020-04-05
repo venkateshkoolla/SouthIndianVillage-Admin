273b078e15697b5cd02de8a55bb12739908a3009
vishwa <venkatesh.koolla@gmail.com>
2020-04-04 21:26:25 -0400

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div class="app">
+      Welcome to SouthIndianVillage
+      Please visit SouthIndianVilalge.ca
+    </div>
+  `
+})
+export class AppComponent {
+
+}
