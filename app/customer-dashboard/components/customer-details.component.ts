4f0cf882b2187dd11e13453e87573f1313335709
Vishwa <venkatesh.koolla@gmail.com>
2020-04-13 18:24:47 -0400

    move html separate

--- app/customer-dashboard/components/customer-details.component.ts
+++ app/customer-dashboard/components/customer-details.component.ts
@@ -4,43 +4,7 @@ import { Customer } from '../models/customer.interface'
 @Component({
     selector : 'customer-details',
     styleUrls : ['customer-details.component.scss'],
-    template : `
-    <div>      
-      <span class = "status" *ngIf = "detail.status == 1"  [class.Active] = "detail.status"></span>
-      <span class = "status" *ngIf = "detail.status == 2"  [class.Enquiry] = "detail.status"></span>
-      <span class = "status" *ngIf = "detail.status == 3"  [class.Closed] = "detail.status"></span>
-      <span class = "status" *ngIf = "detail.status == 4"  [class.Hold] = "detail.status"></span>
-      <div *ngIf = "editing">
-      <input type = "text" 
-      [value] = "detail.firstName"
-      (input) = "OnNameChange(name.value)"
-      #name
-      >
-      </div>
-      <div *ngIf = "!editing">
-      {{detail.firstName}} </div>
-      {{detail.lastName}}
-      {{detail.phoneNumber}}
-      {{detail.postalCode}}
-      {{detail.address}}
-
-      <div *ngIf= "detail.details?.length >0">
-        <div *ngFor = "let customerDetail of detail.details">
-        {{customerDetail.customNotes}}
-      </div>      
-      </div>
-      <div >
-        <div>
-        <button (click)= "toggleEdit()" >
-        {{editing ? 'Done' : 'Edit'}}</button>
-        </div>
-        <div>
-        <button (click)= "OnRemove()" >
-        Remove</button>
-        </div>
-      </div>    
-    </div>
-    `
+    templateUrl : 'customer-details.component.html'
 })
 
 export class CustomerDetailsComponent{
